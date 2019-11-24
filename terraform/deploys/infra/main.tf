c5d9e898b0fbbec1f6067b7c87ef8990f976b6dc
George Norfolk <georgenorfolk@hotmail.co.uk>
2019-11-24 21:32:22 +0000

    Variablised cidr block for vpc.

--- terraform/deploys/infra/main.tf
+++ terraform/deploys/infra/main.tf
@@ -1,3 +1,4 @@
 module "vpc" {
   source = "../../modules/vpc"
+  cidr = "10.0.0.0/16"
 }
--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,66 +1,66 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr
 }
 
 resource "aws_subnet" "elb_az_a" {
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, 0)
+  cidr_block = cidrsubnet(var.cidr, 4, 0)
   map_public_ip_on_launch = true
   availability_zone = "eu-west-1a"
 }
 
 resource "aws_subnet" "elb_az_b" {
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, 1)
+  cidr_block = cidrsubnet(var.cidr, 4, 1)
   map_public_ip_on_launch = true
   availability_zone = "eu-west-1b"
 }
 
 resource "aws_subnet" "elb_az_c" {
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, 2)
+  cidr_block = cidrsubnet(var.cidr, 4, 2)
   map_public_ip_on_launch = true
   availability_zone = "eu-west-1c"
 }
 
 resource "aws_subnet" "app_az_a" {
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, 3)
+  cidr_block = cidrsubnet(var.cidr, 4, 3)
   map_public_ip_on_launch = false
   availability_zone = "eu-west-1a"
 }
 
 resource "aws_subnet" "app_az_b" {
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, 4)
+  cidr_block = cidrsubnet(var.cidr, 4, 4)
   map_public_ip_on_launch = false
   availability_zone = "eu-west-1b"
 }
 
 resource "aws_subnet" "app_az_c" {
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, 5)
+  cidr_block = cidrsubnet(var.cidr, 4, 5)
   map_public_ip_on_launch = false
   availability_zone = "eu-west-1c"
 }
 
 resource "aws_subnet" "db_az_a" {
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, 6)
+  cidr_block = cidrsubnet(var.cidr, 4, 6)
   map_public_ip_on_launch = false
   availability_zone = "eu-west-1a"
 }
 
 resource "aws_subnet" "db_az_b" {
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, 7)
+  cidr_block = cidrsubnet(var.cidr, 4, 7)
   map_public_ip_on_launch = false
   availability_zone = "eu-west-1b"
 }
 
 resource "aws_subnet" "db_az_c" {
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 4, 8)
+  cidr_block = cidrsubnet(var.cidr, 4, 8)
   map_public_ip_on_launch = false
   availability_zone = "eu-west-1c"
 }
