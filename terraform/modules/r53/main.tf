b8f31270a8c9d36b2b2fa9600eb55784e8be811d
George Norfolk <georgenorfolk@hotmail.co.uk>
2019-11-27 00:14:03 +0000

    Adding ns and soa records to tf.

--- terraform/modules/r53/main.tf
+++ terraform/modules/r53/main.tf
@@ -2,27 +2,27 @@ resource "aws_route53_zone" "norfolkgaming" {
   name = "norfolkgaming.com"
 }
 
-# resource "aws_route53_record" "ns" {
-#   zone_id = aws_route53_zone.norfolkgaming.zone_id
-#   name    = "norfolkgaming.com"
-#   type    = "NS"
-#   ttl     = "172800"
-#   allow_overwrite = true
-#   records = [
-#     "ns-332.awsdns-41.com.",
-#     "ns-893.awsdns-47.net.",
-#     "ns-1889.awsdns-44.co.uk.",
-#     "ns-1217.awsdns-24.org."
-#   ]
-# }
-#
-# resource "aws_route53_record" "soa" {
-#   zone_id = aws_route53_zone.norfolkgaming.zone_id
-#   name    = "norfolkgaming.com"
-#   type    = "SOA"
-#   ttl     = "900"
-#   allow_overwrite = true
-#   records = [
-#     "ns-332.awsdns-41.com. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
-#   ]
-# }
+resource "aws_route53_record" "ns" {
+  zone_id = aws_route53_zone.norfolkgaming.zone_id
+  name    = "norfolkgaming.com"
+  type    = "NS"
+  ttl     = "172800"
+  allow_overwrite = true
+  records = [
+    "ns-493.awsdns-61.com.",
+    "ns-1964.awsdns-53.co.uk.",
+    "ns-1070.awsdns-05.org.",
+    "ns-954.awsdns-55.net."
+  ]
+}
+
+resource "aws_route53_record" "soa" {
+  zone_id = aws_route53_zone.norfolkgaming.zone_id
+  name    = "norfolkgaming.com"
+  type    = "SOA"
+  ttl     = "900"
+  allow_overwrite = true
+  records = [
+    "ns-493.awsdns-61.com. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
+  ]
+}
