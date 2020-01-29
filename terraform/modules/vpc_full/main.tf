181591b94908a98daaa154510632eca9efe8cc0f
George Norfolk <norfolgx@C02ZLC0SMD6M.local>
2020-01-29 17:27:04 +0000

    Looping over elb and app route tables and associations.

--- terraform/modules/vpc_full/main.tf
+++ terraform/modules/vpc_full/main.tf
@@ -50,67 +50,35 @@ resource "aws_nat_gateway" "nat" {
   subnet_id = aws_subnet.elb[each.key].id
 }
 
-# resource "aws_route_table" "elb" {
-#   vpc_id = aws_vpc.main.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.main.id
-#   }
-# }
-#
-# resource "aws_route_table_association" "elb_az_a" {
-#   subnet_id      = aws_subnet.elb_az_a.id
-#   route_table_id = aws_route_table.elb.id
-# }
-#
-# resource "aws_route_table_association" "elb_az_b" {
-#   subnet_id      = aws_subnet.elb_az_b.id
-#   route_table_id = aws_route_table.elb.id
-# }
-#
-# resource "aws_route_table_association" "elb_az_c" {
-#   subnet_id      = aws_subnet.elb_az_c.id
-#   route_table_id = aws_route_table.elb.id
-# }
-#
-# resource "aws_route_table" "app_az_a" {
-#   vpc_id = aws_vpc.main.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_nat_gateway.az_a.id
-#   }
-# }
-#
-# resource "aws_route_table_association" "app_az_a" {
-#   subnet_id      = aws_subnet.app_az_a.id
-#   route_table_id = aws_route_table.app_az_a.id
-# }
-#
-# resource "aws_route_table" "app_az_b" {
-#   vpc_id = aws_vpc.main.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_nat_gateway.az_b.id
-#   }
-# }
-#
-# resource "aws_route_table_association" "app_az_b" {
-#   subnet_id      = aws_subnet.app_az_b.id
-#   route_table_id = aws_route_table.app_az_b.id
-# }
-#
-# resource "aws_route_table" "app_az_c" {
-#   vpc_id = aws_vpc.main.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_nat_gateway.az_c.id
-#   }
-# }
-#
-# resource "aws_route_table_association" "app_az_c" {
-#   subnet_id      = aws_subnet.app_az_c.id
-#   route_table_id = aws_route_table.app_az_c.id
-# }
+resource "aws_route_table" "elb" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "elb" {
+  for_each = {a = 0, b = 1, c = 2}
+  subnet_id      = aws_subnet.elb[each.key].id
+  route_table_id = aws_route_table.elb.id
+}
+
+resource "aws_route_table" "app" {
+  for_each = {a = 0, b = 1, c = 2}
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat[each.value].id
+  }
+}
+
+resource "aws_route_table_association" "app" {
+  for_each = {a = 0, b = 1, c = 2}
+  subnet_id      = aws_subnet.elb[each.key].id
+  route_table_id = aws_route_table.app[each.key].id
+}
+
 #
 # resource "aws_route_table" "db" {
 #   vpc_id = aws_vpc.main.id
