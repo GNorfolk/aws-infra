c3866ca1ad6d0a458b1d275911a5026df90c3af7
George Norfolk <norfolgx@C02ZLC0SMD6M.local>
2020-01-29 17:28:52 +0000

    Calling nat identifier properly.

--- terraform/modules/vpc_full/main.tf
+++ terraform/modules/vpc_full/main.tf
@@ -69,7 +69,7 @@ resource "aws_route_table" "app" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat[each.value].id
+    gateway_id = aws_nat_gateway.nat[each.key].id
   }
 }
 
