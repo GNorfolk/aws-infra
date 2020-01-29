5e4d2ec00678a0489d03e0934cec5d89b3f8731a
George Norfolk <norfolgx@C02ZLC0SMD6M.local>
2020-01-29 13:10:32 +0000

    Testing looping over subnets.

--- terraform/modules/vpc_min/main.tf
+++ terraform/modules/vpc_min/main.tf
@@ -5,80 +5,95 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "elb_az_a" {
+resource "aws_subnet" "uhh" {
+  for_each = {
+    elb_az_a = true
+    app_az_a = false
+    db_az_a = false
+  }
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(var.cidr, 4, 0)
-  map_public_ip_on_launch = true
+  cidr_block = cidrsubnet(var.cidr, 4, count)
+  map_public_ip_on_launch = each.value
   availability_zone = "eu-west-1a"
   tags = {
-    Name = "elb_az_a"
+    Name = each.key
   }
 }
 
-resource "aws_subnet" "app_az_a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(var.cidr, 4, 1)
-  map_public_ip_on_launch = false
-  availability_zone = "eu-west-1a"
-  tags = {
-    Name = "app_az_a"
-  }
-}
-
-resource "aws_subnet" "db_az_a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(var.cidr, 4, 2)
-  map_public_ip_on_launch = false
-  availability_zone = "eu-west-1a"
-  tags = {
-    Name = "db_az_a"
-  }
-}
-
-resource "aws_eip" "nat_az_a" {
-  vpc = true
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_nat_gateway" "az_a" {
-  allocation_id = aws_eip.nat_az_a.id
-  subnet_id = aws_subnet.elb_az_a.id
-}
-
-resource "aws_route_table" "elb" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-}
-
-resource "aws_route_table_association" "elb_az_a" {
-  subnet_id      = aws_subnet.elb_az_a.id
-  route_table_id = aws_route_table.elb.id
-}
-
-resource "aws_route_table" "app_az_a" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.az_a.id
-  }
-}
-
-resource "aws_route_table_association" "app_az_a" {
-  subnet_id      = aws_subnet.app_az_a.id
-  route_table_id = aws_route_table.app_az_a.id
-}
-
-resource "aws_route_table" "db" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table_association" "db_az_a" {
-  subnet_id      = aws_subnet.db_az_a.id
-  route_table_id = aws_route_table.db.id
-}
+# resource "aws_subnet" "elb_az_a" {
+#   vpc_id = aws_vpc.main.id
+#   cidr_block = cidrsubnet(var.cidr, 4, 0)
+#   map_public_ip_on_launch = true
+#   availability_zone = "eu-west-1a"
+#   tags = {
+#     Name = "elb_az_a"
+#   }
+# }
+#
+# resource "aws_subnet" "app_az_a" {
+#   vpc_id = aws_vpc.main.id
+#   cidr_block = cidrsubnet(var.cidr, 4, 1)
+#   map_public_ip_on_launch = false
+#   availability_zone = "eu-west-1a"
+#   tags = {
+#     Name = "app_az_a"
+#   }
+# }
+#
+# resource "aws_subnet" "db_az_a" {
+#   vpc_id = aws_vpc.main.id
+#   cidr_block = cidrsubnet(var.cidr, 4, 2)
+#   map_public_ip_on_launch = false
+#   availability_zone = "eu-west-1a"
+#   tags = {
+#     Name = "db_az_a"
+#   }
+# }
+#
+# resource "aws_eip" "nat_az_a" {
+#   vpc = true
+# }
+#
+# resource "aws_internet_gateway" "main" {
+#   vpc_id = aws_vpc.main.id
+# }
+#
+# resource "aws_nat_gateway" "az_a" {
+#   allocation_id = aws_eip.nat_az_a.id
+#   subnet_id = aws_subnet.elb_az_a.id
+# }
+#
+# resource "aws_route_table" "elb" {
+#   vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.main.id
+#   }
+# }
+#
+# resource "aws_route_table_association" "elb_az_a" {
+#   subnet_id      = aws_subnet.elb_az_a.id
+#   route_table_id = aws_route_table.elb.id
+# }
+#
+# resource "aws_route_table" "app_az_a" {
+#   vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.az_a.id
+#   }
+# }
+#
+# resource "aws_route_table_association" "app_az_a" {
+#   subnet_id      = aws_subnet.app_az_a.id
+#   route_table_id = aws_route_table.app_az_a.id
+# }
+#
+# resource "aws_route_table" "db" {
+#   vpc_id = aws_vpc.main.id
+# }
+#
+# resource "aws_route_table_association" "db_az_a" {
+#   subnet_id      = aws_subnet.db_az_a.id
+#   route_table_id = aws_route_table.db.id
+# }
