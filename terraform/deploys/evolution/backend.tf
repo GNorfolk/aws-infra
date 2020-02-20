f50d954cd2a2fc01c7562c8c950fa2afed19a3ec
George Norfolk <norfolgx@C02ZLC0SMD6M.local>
2020-02-20 13:01:38 +0000

    Renaming tfstate file for better readability.

--- terraform/deploys/evolution/backend.tf
+++ terraform/deploys/evolution/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "norfolkgaming-tfstate"
-    key = "aws-flexible.tfstate"
+    key = "aws-evolution.tfstate"
     region = "eu-west-1"
     encrypt = true
   }
