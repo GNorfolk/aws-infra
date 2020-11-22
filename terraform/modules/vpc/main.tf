d5dc51796b77175c0d9d0320044f006e41e886b5
George Norfolk <george.norfolk@lexisnexis.co.uk>
2020-11-22 13:15:15 +0000

    Testing pull id from subnets.

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -42,11 +42,11 @@ resource "aws_internet_gateway" "main" {
   vpc_id  = aws_vpc.main.id
 }
 
-# resource "aws_nat_gateway" "nat" {
-#   for_each      = toset(aws_subnet.app.id)
-#   allocation_id = aws_eip.nat[each.key].id
-#   subnet_id     = each.key
-# }
+resource "aws_nat_gateway" "nat" {
+  for_each      = { subnet.id in aws_subnet.app }
+  allocation_id = aws_eip.nat[each.key].id
+  subnet_id     = each.key
+}
 
 output "output" {
   value = length(aws_subnet.app[*])
