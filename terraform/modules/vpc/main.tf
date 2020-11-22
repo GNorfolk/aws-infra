c5152fd798ba0f4d215380a61178574d3f4cac91
George Norfolk <george.norfolk@lexisnexis.co.uk>
2020-11-22 14:29:43 +0000

    Adding app route table and association.

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -80,7 +80,7 @@ resource "aws_route_table" "app" {
   vpc_id    = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat[idx].id
+    gateway_id = aws_nat_gateway.nat[each.key].id
   }
 }
 
